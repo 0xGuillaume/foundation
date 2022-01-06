7e587082eee84a7efd38d542a2e33568aad68859
rootGuillaume <guillaumegasmi@protonmail.com>
2022-01-07 00:48:13 +0100

    test

--- foundation/main/views.py
+++ foundation/main/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from .models import *
+
 
 # Create your views here.
 def home(request):
     """Index page"""
 
-    return render(request, 'main/index.html')
+    chars = Char.objects.all()
+
+    context = {
+        "chars": chars
+    }
+
+    return render(request, 'main/index.html', context)
